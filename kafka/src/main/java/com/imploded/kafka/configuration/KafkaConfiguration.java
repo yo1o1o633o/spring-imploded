5a977ca960dc9a2b9cbaf04da21796783621f247
shuai.yang <shuai.yang@corp.to8to.com>
2021-05-10 18:21:23 +0800

    Kafka配置类

---
+++ kafka/src/main/java/com/imploded/kafka/configuration/KafkaConfiguration.java
@@ -0,0 +1,10 @@
+package com.imploded.kafka.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author shuai.yang
+ */
+@Configuration
+public class KafkaConfiguration {
+}
