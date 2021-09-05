1363f84baf3e6f054c6add51349ae90199e9e48a
yangs <296077244@qq.com>
2021-09-05 22:51:04 +0800

    启动类

---
+++ complex/src/main/java/com/imploded/complex/ComplexApplication.java
@@ -0,0 +1,13 @@
+package com.imploded.complex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplexApplication.class, args);
+    }
+
+}
